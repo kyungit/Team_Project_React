d40f8e4800456b9129d044a20ee3764ec5baa69a
kyungit <ehrud0226@gmail.com>
2023-12-20 17:04:57 +0900

    23-12-20 17:00 구글로그인

--- src/api/Login/NaverLogin.js
+++ src/api/Login/NaverLogin.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import naverLogin from '../../assets/img/naverLogin.png'
+
 const NaverLogin = () => {
     const Naver_CLIENT_ID = process.env.NAVER_CLIENT_ID // 발급받은 클라이언트 아이디
     const REDIRECT_URI = 'http://localhost:3000/oauth'
@@ -10,7 +12,7 @@ const NaverLogin = () => {
         window.location.href = NAVER_AUTH_URL
     }
 
-    return <button onClick={NaverLogin}>네이버 로그인</button>
+    return <button onClick={NaverLogin}><img src={naverLogin} style={{width:'180px'}}/> </button>
 }
 
 export default NaverLogin
