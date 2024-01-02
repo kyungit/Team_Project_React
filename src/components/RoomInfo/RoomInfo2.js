5feeb19868aa5e97c79e1ff2c9d029d086917871
jingom368 <jingom369@gmail.com>
2024-01-03 01:03:03 +0900

    24-01-03 01:03

--- src/components/RoomInfo/RoomInfo2.js
+++ src/components/RoomInfo/RoomInfo2.js
@@ -5,18 +5,18 @@ import RoomInfoContext from '../../context/RoomInfo_Context'
 
 export default function RoomInfo2() {
     const { roomInfos } = useContext(RoomInfoContext)
-    let firstRoomInfo = null
+    let roomInfo1 = null
     if (roomInfos && roomInfos.roomInfos1) {
-        firstRoomInfo = roomInfos.roomInfos1[0]
+        roomInfo1 = roomInfos.roomInfos1[0]
     }
     return (
         <div className="col-start-3 col-end-11 w-full h-1000">
-            {firstRoomInfo && (
+            {roomInfo1 && (
                 <Column>
                     <Row>More Hotels</Row>
-                    <Row className="text-2xl font-semibold">{firstRoomInfo.d_name}</Row>
+                    <Row className="text-2xl font-semibold">{roomInfo1.d_name}</Row>
                     <Row className="mt-1">**2.5-stars**</Row>
-                    <Row className="mt-1">{firstRoomInfo.d_road}</Row>
+                    <Row className="mt-1">{roomInfo1.d_road}</Row>
                     <Row className="mt-1">/**100,000**</Row>
                     <Row className="mt-1 w-full">
                         **4,5 35 riviews**/
@@ -26,7 +26,7 @@ export default function RoomInfo2() {
                             <div>select a room</div>
                         </Row>
                     </Row>
-                    <Row className="w-1/2 h-96 bg-red-400">**</Row>
+                    <img className="w-1/2 h-80 object-cover rounded-2xl" src={roomInfo1.d_img} alt=""></img>
                 </Column>
             )}
         </div>
